129029d1784aefd999bfe4315d36a4d223cfe5c8
Charles Cruse <charle9@vt.edu>
2023-01-23 17:59:04 -0500

    last

--- enumerables.rb
+++ enumerables.rb
@@ -58,10 +58,14 @@ class Array
 
     def my_flatten
 
-        return [self] if self == !self.to_a?
+        return [self] if self == !self.is_a?(Array)
 
-        
-        (self.length - 1)
+        new_arr = []
+
+        new_arr << self[0]
+        new_arr += self[1..-1].my_flatten
+        # (self.length - 1)
+        new_arr
 
         
 
@@ -99,9 +103,14 @@ a = [1, 2, 3]
 p a.my_reject { |num| num > 1 } # => [1]
 p a.my_reject { |num| num == 4 } # => [1, 2, 3]
 
-p "----------------Question 2----------------"
+p "----------------Question 3----------------"
 
 p a.my_any? { |num| num > 1 } # => true
 p a.my_any? { |num| num == 4 } # => false
 p a.my_all? { |num| num > 1 } # => false
 p a.my_all? { |num| num < 4 } # => true
+
+
+
+p "----------------Question 4----------------"
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
